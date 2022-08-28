019b4c6e75ea122e34cdfcf5e496366e1ac0507a
horsemaker <yash_ghodekar@yahoo.in>
2022-08-28 14:31:03 +0530

    docs: adding Anatomy & TDD to README

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-/** We don't import "test" or "expect". Both "test" & "expect" are methods of Jest that are provided by CRA globally. */
 
-/**
- * test
- *
- * test(name, fn, timeout)
- * - The first argument is the test name used to identify the test.
- * - The second argument is a function that contains the expectations to test.
- * - The third argument is the timeout which is an optional argument for specifying how long to wait before aborting the test. The default timeout value is 5 seconds.
- */
 test("renders learn react link", () => {
   render(<App />);
-  // case insensitive regex with the words "learn react"
   const linkElement = screen.getByText(/learn react/i);
-  // "toBeInTheDocument" => matcher function => carries out assertion
   expect(linkElement).toBeInTheDocument();
 });
