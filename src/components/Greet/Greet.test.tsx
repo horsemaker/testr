bf7579ca5b30a950d3f4de6154470ba6159c85eb
horsemaker <yash_ghodekar@yahoo.in>
2022-08-28 17:36:03 +0530

    test: use describe

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -5,14 +5,32 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+describe("Greet without name", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("This is sample nesting of describe", () => {
+    test("nested test renders correctly", () => {
+      render(<Greet />);
+      const textElement = screen.getByText("Hello");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Yash" />);
-  const textElement = screen.getByText("Hello Yash");
-  expect(textElement).toBeInTheDocument();
+describe("Greet with name", () => {
+  test("renders with the name Yash", () => {
+    render(<Greet name="Yash" />);
+    const textElement = screen.getByText("Hello Yash");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("renders with the name Codevolution", () => {
+    render(<Greet name="Codevolution" />);
+    const textElement = screen.getByText("Hello Codevolution");
+    expect(textElement).toBeInTheDocument();
+  });
 });
