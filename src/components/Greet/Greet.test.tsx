2389e00f1dc91b5628409ee00a5a06c271c6ecd4
horsemaker <yash_ghodekar@yahoo.in>
2022-08-28 14:47:36 +0530

    test: learning TDD

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,8 +1,18 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component it should render hello followed by the name.
+ */
+
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
 test("Greet renders correctly", () => {
   render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
+  const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Yash" />);
+  const textElement = screen.getByText("Hello Yash");
   expect(textElement).toBeInTheDocument();
 });
