f545f093c4ef3088cd3fde067b26b5daebc779b8
horsemaker <yash_ghodekar@yahoo.in>
2022-09-06 00:51:48 +0530

    feat: use getByRole options

--- src/components/Application/Application.test.tsx
+++ src/components/Application/Application.test.tsx
@@ -5,9 +5,23 @@ describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
 
+    // page heading element
+    const pageHading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHading).toBeInTheDocument();
+
+    // section heading element
+    const sectionHeading = screen.getByRole("heading", { level: 2 });
+    expect(sectionHeading).toBeInTheDocument();
+
     // input element
-    const nameElement = screen.getByRole("textbox");
+    const nameElement = screen.getByRole("textbox", { name: "Name" });
     expect(nameElement).toBeInTheDocument();
+
+    // textarea element
+    const bioElement = screen.getByRole("textbox", { name: "Bio" });
+    expect(bioElement).toBeInTheDocument();
 
     // select element/dropdown
     const jobLocationElement = screen.getByRole("combobox");
--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,28 +1,36 @@
 export const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job Location</label>
-        <select id="job-location">
-          <option value="">Select a city</option>
-          <option value="BLR">Bangalore</option>
-          <option value="GGN">Gurgaon</option>
-          <option value="HYD">Hyderabad</option>
-          <option value="MUM">Mumbai</option>
-          <option value="PUNE">Pune</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="terms">
-          <input type="checkbox" id="terms" /> I agree to the terms and
-          conditions.
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea name="bio" id="bio" cols={30} rows={10} />
+        </div>
+        <div>
+          <label htmlFor="job-location">Job Location</label>
+          <select id="job-location">
+            <option value="">Select a city</option>
+            <option value="BLR">Bangalore</option>
+            <option value="GGN">Gurgaon</option>
+            <option value="HYD">Hyderabad</option>
+            <option value="MUM">Mumbai</option>
+            <option value="PUNE">Pune</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="terms">
+            <input type="checkbox" id="terms" /> I agree to the terms and
+            conditions.
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
